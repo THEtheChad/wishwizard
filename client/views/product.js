d438be026d5e70adeb96196bdb0e0a6b9bfd1507
THEtheChad <chad.d.elliott@gmail.com>
2014-07-19 23:50:30 -0500

    a whole bunch more shit

--- client/views/product.js
+++ client/views/product.js
@@ -1,3 +1,13 @@
 Template.product.likes = function(){
+	return Meteor.users.find({_id: {$in: this.likes}}).map(function(doc){
+		return '<img src="' + doc.avatar + '">';
+	}).join('');
+};
 
+Template.product.likeCount = function(){
+	return this.likes.length;
+};
+
+Template.product.price = function(){
+	return this.price.toFixed(2);
 };
