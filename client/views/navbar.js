bb905c46baf4cd1a4fbf64f1eef33ec695dbb84d
Anthony Galati <grig.wyrd@gmail.com>
2014-07-20 00:44:26 -0500

    fixed something in the navbar

--- client/views/navbar.js
+++ client/views/navbar.js
@@ -1,8 +1,12 @@
 Template.navbar.events({
-   "click li": function(e,tmpl) {
+   "click .navbar-item": function(e,tmpl) {
        console.log(arguments);
        tmpl.$(".active").removeClass("active");
        $(e.currentTarget).addClass("active");
+   },
+   "click .navbar-brand": function(e,tmpl){
+       tmpl.$(".active").removeClass("active");
+       $(".default-active").addClass("active");
    }
 });
 
