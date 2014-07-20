2ab04a517f84cfdafba95fa8b268b94d561a027f
Nate Allen <neight.allen@gmail.com>
2014-07-20 00:46:32 -0500

    Might be on my way to a real donate button

--- client/views/donate.js
+++ client/views/donate.js
@@ -1,17 +1,8 @@
-Template.donate.events({'submit' : function(event, template) {
+Template.donate.events({'submit .ajax-submit' : function(event, template) {
     submitDonate();
     event.preventDefault(); //prevent page refresh
 }});
 
-function submitDonate() {
-    form={};
-
-    $.each($('#donateForm').serializeArray(), function() {
-        form[this.name] = this.value;
-    });
-
-    //do validation on form={firstname:'first name', lastname: 'last name', email: 'email@email.com'}
-
-    Meteor.call('donate', form);
-
+Template.donate.visible = function () {
+	return Session.get('donateModalVisible');
 }
